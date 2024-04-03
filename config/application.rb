a2154181bfc85f59f3cff3cb872189b651710c07
takeru0076 <takeru200276@icloud.com>
2024-04-03 13:12:48 +0900

    RSpecの初期設定を完了

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module TechlogApp
     config.generators do |g|
       g.assets false
       g.helper false
+      g.test_framwork :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_spces: false
     end
   end
 end
