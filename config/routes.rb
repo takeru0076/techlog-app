6c8718355584075396baa52dbf968d1e1a155e95
takeru0076 <takeru200276@icloud.com>
2024-04-08 16:12:01 +0900

    学習録削除機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
-  resources :posts, only: %i[new create show index]
+  resources :posts, only: %i[new create show index destroy]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
